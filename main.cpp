d3cd0580b6ec10556c36e1dfe3a6400db24023e5
zihasoo <dlquddbs1234@gmail.com>
2025-09-15 14:41:33 +0900

    코딩중2

--- main.cpp
+++ main.cpp
@@ -5,25 +5,34 @@ using namespace std;
 
 vector<int> adj[1'000'003];
 vector<bool> visited;
-vector<bool> adapter;
+
+void remove_link(int from, int target) {
+    for (int i = 0; i < adj[from].size(); i++)
+    {
+        if(adj[from][i] == target) {
+            adj[from].erase(adj[from].begin() + i);
+            return ;
+        }
+    }
+}
 
 int n;
 int main() {
-    //fio;
+    fio;
     cin >> n;
     visited.assign(n + 3, false);
-    adapter.assign(n + 3, false);
     int u,v;
     for (int i = 0; i < n - 1; i++) {
         cin >> u >> v;
-        adj[u].push_back(u);
-        adj[v].push_back(v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
     queue<int> Q;
     for (int i = 1; i <= n; i++)
     {
         if (adj[i].size() == 1) {
+            visited[i] = true;
             Q.push(i);
         }
     }
@@ -35,24 +44,20 @@ int main() {
         int cur = Q.front();
         Q.pop();
 
-        cout << cur << '\n';
-
-        if (visited[cur]) continue;
-        visited[cur] = true;
-
         for(int ad : adj[cur]) {
             if (visited[ad]) continue;
             ans++;
             visited[ad] = true;
             for(int adad : adj[ad]) {
-                if (!visited[adad]) {
+                if (!visited[adad] && adj[adad].size() == 2) {
+                    remove_link(adad, ad);
+                    visited[adad] = true;
                     Q.push(adad);
                 }
             }
         }
     }
     cout << ans;
-    
 
     return 0;
 }
