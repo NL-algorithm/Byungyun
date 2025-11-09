9eb41efb11d003bc0cffe55081c72184ffe72559
zihasoo <dlquddbs1234@gmail.com>
2025-11-09 22:30:17 +0900

    11월09일 문풀

---
+++ temp/17135(캐슬 디펜스).cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+using namespace std;
+using pr = pair<int, int>;
+
+int n, m, d;
+int origin_board[16][16];
+int archers[4];
+int ans = -1;
+
+#ifndef BOJ
+void print_board(const vector<vector<int>> &target) {
+    cout << "---------------------\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << target[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+#endif
+
+vector<vector<int>> make_copy_board() {
+    vector<vector<int>> copy(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            copy[i][j] = origin_board[i][j];
+        }
+    }
+    return copy;
+}
+
+void move_board(vector<vector<int>> &board) {
+    for (int i = n - 1; i > 0; i--) {
+        board[i] = move(board[i - 1]);
+    }
+    board[0].assign(m, 0);
+}
+
+pr bfs(int sx, int sy, const vector<vector<int>> &board) {
+    static const pr dir[3] = {{0, -1}, {-1, 0}, {0, 1}};
+    vector<vector<int>> dist(n + 1, vector<int>(m, -1));
+    queue<pr> Q;
+
+    dist[sx][sy] = 0;
+    Q.emplace(sx, sy);
+
+    while (!Q.empty()) {
+        auto [x, y] = Q.front();
+        Q.pop();
+        for (int i = 0; i < 3; i++) {
+            int nx = x + dir[i].first;
+            int ny = y + dir[i].second;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+            if (dist[nx][ny] != -1 || dist[x][y] + 1 > d)
+                continue;
+            dist[nx][ny] = dist[x][y] + 1;
+            if (board[nx][ny] == 1) {
+                return {nx, ny};
+            }
+            Q.emplace(nx, ny);
+        }
+    }
+    return {-1, -1};
+}
+
+int trial(vector<vector<int>> &board) {
+    set<pr> buffer;
+    int ret = 0;
+    for (int i = 1; i <= 3; i++) {
+        auto can = bfs(n, archers[i], board);
+        if (can.first != -1) {
+            buffer.insert(can);
+        }
+    }
+
+    for (auto &[x, y] : buffer) {
+        board[x][y] = 0;
+        ret++;
+    }
+
+    return ret;
+}
+
+int simul() {
+    int ret = 0;
+    auto copy_board = make_copy_board();
+    for (int i = 0; i < n; i++) {
+        ret += trial(copy_board);
+        move_board(copy_board);
+    }
+    return ret;
+}
+
+void back(int idx) {
+    if (idx == 4) {
+        ans = max(ans, simul());
+        return;
+    }
+    for (int i = archers[idx - 1] + 1; i < m - (3 - idx); i++) {
+        archers[idx] = i;
+        back(idx + 1);
+    }
+}
+
+int main() {
+    fio;
+    cin >> n >> m >> d;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> origin_board[i][j];
+        }
+    }
+    archers[0] = -1;
+    back(1);
+    cout << ans;
+    return 0;
+}
+
+// 궁수 배치 경우의 수: 15 * 14 * 13 / 6 = 5 * 7 * 13 = 455
+// 적 시뮬레이션 최대 횟수: 15
+// 각 시뮬레이션당 연산 횟수: 대충 225
+// 총합 150만정도
